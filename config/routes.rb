32365feb4ae5e1a95b0b99a5115ffd1f247372a1
shoichi.kikuchi <bcaf.psvclluh.ahdb@gmail.com>
2019-02-08 04:44:42 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+  
+  get 'ranking/want', to: 'ranking#want'
+  get 'ranking/have', to: 'ranking#have'
+  
   resources :users, only: [:show, :new, :create]
   
   resources :items, only: [:show, :new]
